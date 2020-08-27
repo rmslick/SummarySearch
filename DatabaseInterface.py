9407c1838d32efd73831e41e6a02d5176083d768
rmslick <rmslick@buffalo.edu>
2020-08-26 23:08:34 -0400

    Derp level 10 on filenames. Fixed.

--- DatabaseInterface.py
+++ DatabaseInterface.py
@@ -1,51 +1,55 @@
-import json
 import torch
-import json 
-from transformers import T5Tokenizer, T5ForConditionalGeneration, T5Config
+from transformers import BertTokenizer, BertModel, BertForMaskedLM
+from sentence_transformers import *
+import scipy.spatial
 import pymongo
 
-model = T5ForConditionalGeneration.from_pretrained('t5-small')
-tokenizer = T5Tokenizer.from_pretrained('t5-small')
-device = torch.device('cpu')
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+embedder = SentenceTransformer('bert-base-nli-mean-tokens')
 
-#Single item intry
-def InsertItem(dictionary):
-    mydb = myclient["Papers"]
-    #collection
-    mycol = mydb["entries"]
-    x = mycol.insert_one(dictionary)
 
-def Summarize(body):
-    t5_prepared_Text = "summarize: "+body
-    tokenized_text = tokenizer.encode(t5_prepared_Text, return_tensors="pt").to(device)
-    # summmarize 
-    summary_ids = model.generate(tokenized_text,
-                                        num_beams=4,
-                                        no_repeat_ngram_size=2,
-                                        min_length=30,
-                                        max_length=200,
-                                        early_stopping=True)
-    output = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
-    return output
-# Load json file
-#{location, title, author, summary ,fulltext} = search fields
-def LoadDataJSON(filepath):
-    f = open(filepath)
-    # returns JSON object as  
-    # a dictionary 
-    data = json.load(f)
-    for i in range(5):
-        # Iterating through the json list
-        author =  data[i]["author"]
-        body = data[i]["summary"]
-        body = body.replace('\n','')
-        #Obtain summary
-        summary = Summarize(body)
-        #Build dictionary
-        entry = {"_id":i+1,"author":author,"summary":summary,"fullBody":body}        
-        InsertItem(entry)
-    f.close()
-dataPath = "Data/arxivData.json"
-LoadDataJSON(dataPath)
-myclient.close()
+class DataBaseInterface:
+    def __init__(self):
+        self.iDs = []
+        self.corpus = []
+        self.myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+        self.database = self.myclient["Papers"]
+        #collection
+        self.collection = self.database["entries"]
+        self.FillCorpus()
+
+    def FillCorpus(self):
+        for entry in self.collection.find():
+            self.iDs.append(entry["_id"])
+            self.corpus.append(entry["summary"])
+            #print("Pushing back:\n      Id - " + str(entry["_id"]) + " \n      Summary - " + entry["summary"])
+    # Frequency update of database
+    def Update(self):
+        pass
+    # Takes in a project description and returns relevant corpus (lessons)
+    # Sends top summaries to the screen
+    # returns list of id's ordered by relevancy
+    def SearchCorpus(self,inputQuery,closest_n=3):
+        test_sentences = [inputQuery]
+        corpus_embeddings = embedder.encode(self.corpus)
+        query_embeddings = embedder.encode(test_sentences)
+
+        topObjIds = []
+        for query, query_embedding in zip(test_sentences, query_embeddings):
+            distances = scipy.spatial.distance.cdist([query_embedding], corpus_embeddings, "cosine")[0]
+
+            results = zip(range(len(distances)), distances)
+            results = sorted(results, key=lambda x: x[1])
+
+            print("\n\n======================\n\n")
+            print("Query:", query)
+            print("\nTop %s most similar sentences in corpus:\n" % closest_n)
+
+            for idx, distance in results[0:closest_n]:
+                index = self.corpus.index(self.corpus[idx])
+                topObjIds.append(self.iDs[index])
+                print(self.corpus[idx].strip(), "(Score: %.4f)" % (1-distance))
+        return topObjIds
+
+database = DataBaseInterface()
+x = database.SearchCorpus("n Natural Language Processing (NLP), it is hard to predict ifsharing will lead to improvements, particularly if tasks are only looselyrelated. To overcome this, we introduce Sluice Networks")
+print(x)
