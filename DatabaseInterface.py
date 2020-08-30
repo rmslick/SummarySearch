e5845b3defed0bfa03ad00ec4e0c4a38ce690b4a
rmslick <rmslick@buffalo.edu>
2020-08-30 19:33:29 -0400

    Added main menu for testing and an add method. Added some ehh to the README.

--- DatabaseInterface.py
+++ DatabaseInterface.py
@@ -3,6 +3,7 @@ from transformers import BertTokenizer, BertModel, BertForMaskedLM
 from sentence_transformers import *
 import scipy.spatial
 import pymongo
+from transformers import T5Tokenizer, T5ForConditionalGeneration, T5Config
 
 embedder = SentenceTransformer('bert-base-nli-mean-tokens')
 
@@ -16,24 +17,42 @@ class DataBaseInterface:
         #collection
         self.collection = self.database["entries"]
         self.FillCorpus()
-
+        #For summarization
+        self.model = T5ForConditionalGeneration.from_pretrained('t5-small')
+        self.tokenizer = T5Tokenizer.from_pretrained('t5-small')
+        self.device = torch.device('cpu')
+    #Called to synch instance with database
     def FillCorpus(self):
         for entry in self.collection.find():
             self.iDs.append(entry["_id"])
             self.corpus.append(entry["summary"])
             #print("Pushing back:\n      Id - " + str(entry["_id"]) + " \n      Summary - " + entry["summary"])
-    # Frequency update of database
-    def Update(self):
-        pass
-    # Takes in a project description and returns relevant corpus (lessons)
-    # Sends top summaries to the screen
-    # returns list of id's ordered by relevancy
+    
+    # SummarizeDB
+    def SummarizeEntry(self, fullBody):
+        t5PreparedText = "summarize: "+ fullBody
+        tokenizedText =  self.tokenizer.encode(t5PreparedText, return_tensors="pt").to(self.device)
+        summaryIds = self.model.generate(tokenizedText,
+                                        num_beams=4,
+                                        no_repeat_ngram_size=2,
+                                        min_length=30,
+                                        max_length=200,
+                                        early_stopping=True)
+        summary = self.tokenizer.decode(summaryIds[0], skip_special_tokens=True)
+        return summary
+    def AddLesson(self, author, fullBody):
+        _id = len(self.corpus) + 1
+        summary = self.SummarizeEntry(fullBody)
+        self.corpus.append(summary) #update corpus
+        dictionary = {"_id":_id, "author":author,"summary":summary,"fullBody":fullBody}
+        self.collection.insert_one(dictionary) #Fill DB
     def SearchCorpus(self,inputQuery,closest_n=3):
         test_sentences = [inputQuery]
         corpus_embeddings = embedder.encode(self.corpus)
         query_embeddings = embedder.encode(test_sentences)
 
         topObjIds = []
+        # Corpus seearch
         for query, query_embedding in zip(test_sentences, query_embeddings):
             distances = scipy.spatial.distance.cdist([query_embedding], corpus_embeddings, "cosine")[0]
 
@@ -51,5 +70,16 @@ class DataBaseInterface:
         return topObjIds
 
 database = DataBaseInterface()
-x = database.SearchCorpus("n Natural Language Processing (NLP), it is hard to predict ifsharing will lead to improvements, particularly if tasks are only looselyrelated. To overcome this, we introduce Sluice Networks")
-print(x)
+while True:
+    choice = input("Enter an option 1-5\n1) Look up\n2) Smart Search: Enter a project description \n3) Add a lesson \n4) Risk Analysis \n5) NASA Problems \n6) Visualize")
+    if choice == "1":
+        pass
+    elif choice == "2":
+        description = input("Enter a project description")
+        x = database.SearchCorpus(description)
+        print(x)
+    elif choice == "3":
+        author = input("Enter an author")
+        fullBody = input("Enter a full body")
+        database.AddLesson(author,fullBody)
+    print(x)
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 Link to Sample Data Used for ML: https://www.kaggle.com/neelshah18/arxivdataset/notebooks
+
+Targeted Platform: Ubuntu 20.04
+Dependencies:
+- mongo db (v. 4.4.0)
+- 
+
+InitializeDB: A set of data-specific functions to fill an existing database.  It is assumed database is on and running on the same platform as InitializeDB.
+
+
